a51bdc39f1475531b8ce5afe7368a049296fb20f
rafaelbcastilhos <rafaelbcastilhos@gmail.com>
2022-04-09 13:13:59 -0300

    add grindsome configs

---
+++ gridsome.config.js
@@ -0,0 +1,62 @@
+const tailwindcss = require('tailwindcss');
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+  siteName: 'Rafael B. Castilhos',
+  siteDescription: 'Postagens de Rafael B. Castilhos',
+  siteUrl: 'https://rafaelbcastilhos.github.io/',
+  pathPrefix: '/',
+
+  templates: {
+    Post: '/post/:path',
+    Tag: '/tag/:id',
+  },
+
+  plugins: [
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        path: 'post/**/*.md',
+        typeName: 'Post',
+        refs: {
+          tags: {
+            typeName: 'Tag',
+            create: true,
+          },
+        },
+        remark: {
+          plugins: [
+            [
+              'gridsome-plugin-remark-shiki',
+              { theme: 'nord', skipInline: true },
+            ],
+          ],
+        },
+      },
+    },
+    {
+      use: 'gridsome-plugin-tailwindcss',
+    },
+    {
+      use: '@gridsome/plugin-sitemap',
+    },
+  ],
+
+  transformers: {
+    remark: {
+      autolinkClassName: 'icon icon-link heading-anchor',
+      externalLinksTarget: '_blank',
+      externalLinksRel: ['noopener'],
+      anchorClassName: 'icon icon-link',
+    },
+    plugins: ['@gridsome/remark-prismjs'],
+  },
+
+  css: {
+    loaderOptions: {
+      postcss: {
+        plugins: [tailwindcss, autoprefixer],
+      },
+    },
+  },
+};
