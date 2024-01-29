967f34ac1cc63719cdf914ad6fa565f4f7e7bffa
Rafael Castilhos <rafael.castilhos@sparkag.com.br>
2024-01-29 08:56:15 -0300

    update version

--- src/main.js
+++
@@ -1,6 +0,0 @@
-// import 'prismjs/themes/prism.css';
-import DefaultLayout from '~/layouts/Default.vue';
-
-export default function(Vue, { router, head, isClient }) {
-  Vue.component('Layout', DefaultLayout);
-}
